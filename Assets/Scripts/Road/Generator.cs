69c0430fdd62669d9cfbbfbe298724bda52dd67a
DarkBeer0 <baphometmarx@gmail.com>
2021-10-13 12:03:55 +0200

    RoadGenerator Complete.

--- Assets/Scripts/Road/Generator.cs
+++ Assets/Scripts/Road/Generator.cs
@@ -1,20 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Generator : MonoBehaviour
 {
     [SerializeField] private Transform level;
+    private static DateTime lastUpdate;
+    private static List<Transform> roadList = new List<Transform>();
     private void Awake()
     {
         SpawnRoadPart(new Vector3(20, 0));
         SpawnRoadPart(new Vector3(20, 0) + new Vector3(20,0));
-        SpawnRoadPart(new Vector3(20, 0) + new Vector3(20,0)); //x - 0.34 позиция примерной равности блоков
+        SpawnRoadPart(new Vector3(40, 0) + new Vector3(20,0)); //y - 0.34 позиция примерной равности блоков
     }
 
     private void SpawnRoadPart(Vector3 spawnPosition)
     {
-        Instantiate(level, spawnPosition, Quaternion.identity);
+        roadList.Add(Instantiate(level, spawnPosition, Quaternion.identity));
+        
+    }
+
+    public void Update()
+    {
+        if (DateTime.Now.Subtract(lastUpdate).TotalMilliseconds < 2000)
+            return;
+        #region MoveRoad
+
+        Transform lastRoadObject = roadList.Last();
+        roadList.First().position = new Vector3(lastRoadObject.position.x + 20, lastRoadObject.position.y, lastRoadObject.position.z);
+        Transform bufferRoad = roadList.First();
+
+        roadList.Add(roadList.First());
+        roadList.Remove(bufferRoad);
+
+        #endregion
+
+        // Last road pos = bufferRoad
+
+
+        if (Random.Range(0, 5) < 2)
+        {
+            Debug.Log("Monstr at: " + bufferRoad.position);
+            // TODO
+        }
+
+        Debug.Log("Update");
+
+        lastUpdate = DateTime.Now;
+
     }
 
 }
